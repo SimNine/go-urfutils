d295825b6978b68722dd8d6302a6fadf2210c538
Chris <c.urffer@live.com>
2025-08-29 16:51:42 -0400

    added Union and Intersection functions to Bounds

--- src/geom/bounds.go
+++ src/geom/bounds.go
@@ -31,3 +31,35 @@ func (b Bounds[N]) Contains(p Pos[N]) bool {
 	return p.X >= b.Pos.X && p.X < p2.X &&
 		p.Y >= b.Pos.Y && p.Y < p2.Y
 }
+
+func (b Bounds[N]) Union(other Bounds[N]) Bounds[N] {
+	minX := min(b.Pos.X, other.Pos.X)
+	minY := min(b.Pos.Y, other.Pos.Y)
+	thisOpp := b.OppositeCorner()
+	otherOpp := other.OppositeCorner()
+	maxX := max(thisOpp.X, otherOpp.X)
+	maxY := max(thisOpp.Y, otherOpp.Y)
+
+	return MakeBoundsFromPosAndPos2(
+		MakePosFromTuple(minX, minY),
+		MakePosFromTuple(maxX, maxY),
+	)
+}
+
+func (b Bounds[N]) And(other Bounds[N]) (Bounds[N], bool) {
+	minX := max(b.Pos.X, other.Pos.X)
+	minY := max(b.Pos.Y, other.Pos.Y)
+	thisOpp := b.OppositeCorner()
+	otherOpp := other.OppositeCorner()
+	maxX := min(thisOpp.X, otherOpp.X)
+	maxY := min(thisOpp.Y, otherOpp.Y)
+
+	if minX >= maxX || minY >= maxY {
+		return Bounds[N]{}, false
+	}
+
+	return MakeBoundsFromPosAndPos2(
+		MakePosFromTuple(minX, minY),
+		MakePosFromTuple(maxX, maxY),
+	), true
+}
